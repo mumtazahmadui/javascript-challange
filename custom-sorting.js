4eb7b0f7077f6ee8e16ed37767cbe8f024952e12
Mumtaz Ahmad <37632981+mumtazahmadui@users.noreply.github.com>
2019-02-06 12:58:34 +0530

    Create custom-sorting.js

---
+++ custom-sorting.js
@@ -0,0 +1,21 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+// write a function that sorts the students by name ascending alphabetically, if there are multiple same names then sort by descending age.
+
+//sort by name
+students.sort(function(a, b) {
+  if (a.name < b.name) {
+    return -1;
+  }
+  if (a.name > b.name) {
+    return 1;
+  }
+  return b.age - a.age;
+});
+
+console.log(students.sort());
