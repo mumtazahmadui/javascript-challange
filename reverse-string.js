783320e4aa5b67d7b931b44a4efc362ceefa94f0
Mumtaz Ahmad <37632981+mumtazahmadui@users.noreply.github.com>
2019-02-16 21:27:33 +0530

    Update reverse-string.js

--- reverse-string.js
+++ reverse-string.js
@@ -1,4 +1,28 @@
-const reverseString = (number) => {
+/**
+ * Number Algorithms 
+ */
+
+ /**
+  * REVERSE STRING 
+  * Given an integer return the reverse of the given number 
+  * Input => 321
+  * Output => 123 
+  * Input => -452 
+  * Output => -254
+  * Make sure the number doesn't overflow if it does return 0 
+  * Pseudocode 
+  *  create a variable to hold the reverse number 
+  *  convert the input to string 
+  *  iterate over the string input starting at position 1 
+  *    for each number add them to the front of the variable holding the number
+  *  if the input string first character is equal to - 
+  *    add the element to the front 
+  *  else push the element 
+  *  join the variable holding the new number and convert back to number 
+  *  return the variable  
+  */
+
+  const reverseString = (number) => {
     let newNumber = []; 
     number = number.toString(); 
     for (let i = 1; i < number.length; i++) {
@@ -15,5 +39,4 @@ const reverseString = (number) => {
     }
     return newNumber;
   };
-
   reverseString(8798)
