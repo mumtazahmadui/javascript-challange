ddd0e733b4677f78f5d3040ec01950f3463f2631
Mumtaz Ahmad <37632981+mumtazahmadui@users.noreply.github.com>
2019-02-28 16:59:12 +0530

    Create missing-number.js

---
+++ missing-number.js
@@ -0,0 +1,8 @@
+// missing in sequence only 1 value
+// formulat (n+1) * (n+2) / 2 - total value
+const a = [1,3,4,2,6]; //1 missing
+const b = a.length;
+
+const sum = a.reduce((a,b) => a+b);
+let missing = (b+1) * (b+2) / 2 - sum
+console.log(missing)
