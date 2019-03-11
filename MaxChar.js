faa41f890e01d3ee6b960c3eea8801f523b778bd
Mumtaz Ahmad <37632981+mumtazahmadui@users.noreply.github.com>
2019-03-11 16:43:15 +0530

    Update MaxChar.js

--- MaxChar.js
+++ MaxChar.js
@@ -5,6 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
+const arr = [1,2,3,4,3,6];
+
+let obj = {};
+for(let i of arr) {
+  if(!obj[i]) {
+    obj[i] = 0
+  }
+  obj[i]++;
+}
+
+let char='';
+let maxChar = 0;
+for(let i in obj){
+  if(obj[i] > maxChar) {
+    maxChar = obj[i];
+    char = i;
+  }
+}
+console.log(obj);
+console.log(char)
+
+
+//second approach
 function maxChar(str) {
   let obj = {},
     max = 0,
